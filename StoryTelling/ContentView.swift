eee3af2687f2a55cf1faea6bb56eaa3669b515ef
Goutham <goutham.valmiki@gmail.com>
2026-08-09 17:09:05 +0530

    Initial commit

---
+++ StoryTelling/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StoryTelling
+//
+//  Created by Goutham on 09/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StoryTelling/StoryTellingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryTellingApp.swift
+//  StoryTelling
+//
+//  Created by Goutham on 09/08/26.
+//
+
+import SwiftUI
+
+@main
+struct StoryTellingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
